e341bf6469ad89d47cc47c34a907ab9b831b0069
Mellivora capensis <applessdh@gmail.com>
2025-07-29 01:35:38 +0900

    Fix: Remove unused bcrypt import to fix deployment error

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
-import bcrypt
 from dotenv import load_dotenv
 from sqlalchemy import func # Added for func.date
 
