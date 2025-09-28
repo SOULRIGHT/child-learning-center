c4e48c5a28c7a6b227fb6f4e21f798aebc86263d
Mellivora capensis <applessdh@gmail.com>
2025-09-29 00:24:18 +0900

    자자

--- table_del.py
+++ table_del.py
@@ -1,12 +1,6 @@
 from app import app, db
-from sqlalchemy import text
 
 with app.app_context():
-    # 외래키 제약을 고려한 삭제 순서
-    db.session.execute(text('DELETE FROM learning_record'))
-    db.session.execute(text('DELETE FROM daily_points'))
-    db.session.execute(text('DELETE FROM points_history'))
-    db.session.execute(text('DELETE FROM child'))
-    db.session.execute(text('DELETE FROM "user"'))
-    db.session.commit()
-    print('✅ 모든 데이터 삭제 완료!')
+    db.drop_all()
+    db.create_all()
+    print('✅ 테이블 재생성 완료!')
